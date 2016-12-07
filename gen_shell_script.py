564775218c456c10c660c991452d42e41d2425ab
pwhitetj <pewhite@fcps.edu>
2016-12-06 23:54:34 -0500

    This branch generates shell scripts to run tat
    Not sure if anything is changed besides gen_shell, etc

--- gen_shell_script.py
+++ gen_shell_script.py
@@ -8,7 +8,7 @@ xstart = 20
 ystart = 20
 xwidth = 310
 ywidth = 350
-for xc in range(5):
+for xc in range(6):
     for yc in range(3):
-        print("python3 ttt.py %i %i &" % (xstart+xc*xwidth, ystart+yc*ywidth))
+        print("python3 ttt.py %i %i random random &" % (xstart+xc*xwidth, ystart+yc*ywidth))
         
