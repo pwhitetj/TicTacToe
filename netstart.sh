fbd59d3ce9cb2c4279c4e693ac4c2b448736e41f
pwhitetj <pwhite@tj.com>
2016-12-07 20:22:53 -0500

    Socket shell scripts

---
+++ netstart.sh
@@ -0,0 +1,24 @@
+python3 ttt.py 20 20 random server 5010 &
+sleep 0.3
+python3 ttt.py 20 370 random server 5011 &
+sleep 0.3
+python3 ttt.py 20 720 random server 5012 &
+sleep 0.3
+python3 ttt.py 330 20 random server 5013 &
+sleep 0.3
+python3 ttt.py 330 370 random server 5014 &
+sleep 0.3
+python3 ttt.py 330 720 random server 5015 &
+sleep 0.3
+python3 ttt.py 640 20 random server 5016 &
+sleep 0.3
+python3 ttt.py 640 370 random server 5017 &
+sleep 0.3
+python3 ttt.py 640 720 random server 5018 &
+sleep 0.3
+python3 ttt.py 950 20 random server 5019 &
+sleep 0.3
+python3 ttt.py 950 370 random server 5020 &
+sleep 0.3
+python3 ttt.py 950 720 random server 5021 &
+sleep 0.3
