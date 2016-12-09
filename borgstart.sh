be263199f005c84085200d2c34f02e3687e131ac
Patrick White <pewhite@eich.csl.tjhsst.edu>
2016-12-09 08:35:19 -0500

    Adding shell scripts

---
+++ borgstart.sh
@@ -0,0 +1,24 @@
+python3 ttt.py 20 20 random server 50010 borg &
+sleep 0.3
+python3 ttt.py 20 370 random server 50011 borg &
+sleep 0.3
+python3 ttt.py 20 720 random server 50012 borg &
+sleep 0.3
+python3 ttt.py 330 20 random server 50013 borg &
+sleep 0.3
+python3 ttt.py 330 370 random server 50014 borg &
+sleep 0.3
+python3 ttt.py 330 720 random server 50015 borg &
+sleep 0.3
+python3 ttt.py 640 20 random server 50016 borg &
+sleep 0.3
+python3 ttt.py 640 370 random server 50017 borg &
+sleep 0.3
+python3 ttt.py 640 720 random server 50018 borg &
+sleep 0.3
+python3 ttt.py 9500 20 random server 50019 borg &
+sleep 0.3
+python3 ttt.py 9500 370 random server 50020 borg &
+sleep 0.3
+python3 ttt.py 9500 720 random server 50021 borg &
+sleep 0.3
